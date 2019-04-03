a1a6eaadd34464021b448eacc2f50e2af2c26dfd
Gabriel <gmc2@cin.ufpe.br>
2019-04-03 07:20:48 -0300

    Add files via upload

--- src/methods/Adam_bashfort_por_euler.py
+++ src/methods/Adam_bashfort_por_euler.py
@@ -19,24 +19,33 @@ t, y = symbols("t y")
 print('Metodo de Euler')
 f=[]
 print('h = '+str(h))
-for i in range(1, 7):
+print('t0 = '+str(t0))
+for i in range(0, 5):
+   
+    print (str(i) + ' ' + str(y0))
     k1 = func.subs([(y,y0),(t,t0)])
     y0 = y0+h*k1
-    t0=t0+h
     f.append(y0)
-    print (str(i) + ' ' + str(y0))
+    t0=t0+h
+    
+    
 
-print (f[5])
-for i in range(7, n+1):
-    y_prox = f[5] + h*(((4277/1440)*func.subs([(y,f[5]),(t,t0)])) - ((2641/480)*func.subs([(y,f[4]),(t,t0-1)])) + ((4991/720)*func.subs([(y,f[3]),(t,t0-2)])) - ((3649/720)*func.subs([(y,f[2]),(t,t0-3)])) + ((959/480)*func.subs([(y,f[1]),(t,t0-4)])) - ((95/288)*func.subs([(y,f[0]),(t,t0-5)])))
+
+for i in range(6, n+1):
+    k0 = (1901/float(720))*func.subs([(y,f[4]),(t,t0)])
+    k1 = (1387/float(360))*func.subs([(y,f[3]),(t,t0-h)])
+    k2 = (109/float(30))*func.subs([(y,f[2]),(t,t0-h*2)])
+    k3 = (637/float(360))*func.subs([(y,f[1]),(t,t0-h*3)])
+    k4 = (251/float(720))*func.subs([(y,f[0]),(t,t0-h*4)])
+    y_prox = f[4] + h*(k0 - k1 + k2 - k3 + k4)
     f[0] = f[1]
     f[1] = f[2]
     f[2] = f[3]
     f[3] = f[4]
-    f[4] = f[5]
-    f[5] = y_prox
-    t0=t0+h
+    f[4] = y_prox
     print (str(i) + ' ' + str(y_prox))
+    t0=t0+h
+    
 
 
 
