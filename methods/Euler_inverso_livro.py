d1a9c81cf7bdd9d7aad2ae901a28cfbcb8bf6322
Gabriel Maciel <gmc2@cin.ufpe.br>
2019-04-06 18:58:46 -0300

    Add files via upload

---
+++ methods/Euler_inverso_livro.py
@@ -0,0 +1,32 @@
+from mpmath import *
+from sympy import *
+
+arq = open('entrada.txt', 'r')
+
+texto = arq.readline()
+arq.close()
+
+
+lista = texto.split()
+
+
+print(lista[5])
+
+y0, t0, h, n, func = sympify(lista[1]), sympify(lista[2]), sympify(lista[3]), sympify(lista[4]), sympify(lista[5])
+t, y = symbols("t y")
+
+
+print('Metodo de Euler')
+
+print('h = '+str(h))
+for i in range(1, n+1):
+    k1 = func.subs([(y,y0),(t,t0)])
+    k2 = func.subs([(y,y0+h*k1),(t,t0+h)])
+    y0 = y0+h*k2
+    t0=t0+h
+    print(str(i) + ' ' + str(y0))
+print('\n\n')
+
+
+
+
